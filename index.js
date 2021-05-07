f1b27fb5c2398468e7e3410beeee2cc523ab1133
Sludke <46661484+xdsurvivor@users.noreply.github.com>
2021-05-07 20:56:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+        //PGerar pdf
+        const NomeDono = document.getElementById("nome");
+        const NomePet = document.getElementById("nomeP");
+        const submitBtn = document.getElementById("submitBtn");
+        const { PDFDocument, rgb, degrees } = PDFLib;
+        
+        
+        submitBtn.addEventListener("click", () => {
+            const val = NomeDono.value;
+            const val2 = NomePet.value;
+            if (val.trim() !== "" &amp;&amp; NomeDono.checkValidity() &amp;&amp; NomePet.checkValidity) {
+                // console.log(val);
+                generatePDF(val,val2);
+              } else {
+                NomeDono.reportValidity();
+                NomePet.reportValidity();
+                
+              }
+        });
+        const generatePDF = async (name) => {
+            const existingPdfBytes = await fetch("https://d335luupugsy2.cloudfront.net/cms/files/207518/1620228742certificado-mae_de_pet-banzoo.pdf").then((res) =>
+              res.arrayBuffer()
+            );
+        
+            
+            const pdfDoc = await PDFDocument.load(existingPdfBytes);
+            pdfDoc.registerFontkit(fontkit);
+        
+            
+          //get font
+          const fontBytes = await fetch("https://fonts.gstatic.com/s/opensans/v18/mem8YaGs126MiZpBA-UFW50bbck.woff2").then((res) =>
+          res.arrayBuffer()
+        );
+          
+          const AESTHETICFont  = await pdfDoc.embedFont(fontBytes);
+           // Get the first page of the document
+           const pages = pdfDoc.getPages();
+           const firstPage = pages[0];
+         
+           
+           firstPage.drawText(name, {
+             x: 190,
+             y: 260,
+             size: 64,
+             font: AESTHETICFont,
+               color: rgb(0, 0.118, 0.42),
+               });
+           
+           firstPage.drawText(NomePet.value, {
+               x: 480,
+               y: 60,
+               size: 108,
+               font: AESTHETICFont,
+               color: rgb(0, 0.118, 0.42),
+           });
+         
+          const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+          saveAs(pdfDataUri,"Certificado.pdf")
+        };
