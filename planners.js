d5576b3ea445729d9d0a71f8b863468701a04ca1
Sludke <46661484+xdsurvivor@users.noreply.github.com>
2021-10-20 15:15:20 -0300

    Update planners.js

--- planners.js
+++ planners.js
@@ -57,8 +57,8 @@ const generatePDF = async (name) => {
  
    
    firstPage.drawText(name, {
-     x: 100,
-     y: 900,
+     x: 0,
+     y: 0,
      size: 64,
      font: OpenSans,
        color: rgb(0, 0, 0),
